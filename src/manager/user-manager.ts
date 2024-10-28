379ae8a2b845400e72d4abf65e5226e1815c77a5
OyakXD <kayovieira157@gmail.com>
2024-10-28 20:06:11 -0300

    fixing day

--- src/manager/user-manager.ts
+++ src/manager/user-manager.ts
@@ -139,7 +139,7 @@ export class UserManager {
 
     const currentDay = Utils.getCurrentDate().getDay();
 
-    if (currentDay === 0) {
+    if (currentDay === 0 || currentDay == 2) {
       const receiveNotificationPrivate =
         this.canReceiveNotificationInPrivateChat();
       const users = await this.getUsers();
