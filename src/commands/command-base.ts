917caa3fbd1ae9489e1be30e4264add9dc4f1139
Joãosvc <joaosvc.dev@gmail.com>
2026-06-12 13:58:50 -0300

    feat: update

--- src/commands/command-base.ts
+++ src/commands/command-base.ts
@@ -93,6 +93,7 @@ export class CommandHandler {
             participantID,
             groupParticipant,
             groupParticipants,
+            isParticipantAdmin,
             socket
         };
 
